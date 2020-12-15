2038287c22b09d8259474f01b33737ea2306663d
omar mokhtar <o.attia@nhc.sa>
2020-12-15 10:42:02 +0300

    simple filter
    simple filter

--- js/main.js
+++ js/main.js
@@ -31,6 +31,13 @@ var vm = new Vue({
         description: "The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions have a prominent mane. With a typical head-to-body length of 184–208 cm (72–82 in) they are larger than females at 160–184 cm (63–72 in). It is a social species, forming groups called prides. A lion pride consists of a few adult males, related females and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not.",
         url: "https://en.wikipedia.org/wiki/Lion",
 
+        benificarySearch: "",
+        benificary: [
+
+            'Omar',
+            'zakaria',
+            'qwertyuiop;khfdsazzccvbbnnm',
+        ]
     },
     methods: {
         lionInfo: function () {
@@ -44,6 +51,15 @@ var vm = new Vue({
 
         },
 
+    },
+    computed: {
+        filterbenificary: function () {
+            var filtering = new RegExp(this.benificarySearch, 'i');
+            return this.benificary.filter(function (el) {
+
+                return el.match(filtering)
+            })
+        }
     }
 })
 
